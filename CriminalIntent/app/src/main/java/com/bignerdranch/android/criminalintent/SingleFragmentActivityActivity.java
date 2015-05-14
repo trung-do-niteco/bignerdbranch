ad2084bcde6174a8b687ea1ed21cc106123cb6ec
trungdo <trung.do@niteco.se>
2015-05-14 12:05:08 +0700

    test SmartGit

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivityActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivityActivity.java
@@ -1,12 +1,9 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public abstract class SingleFragmentActivityActivity extends FragmentActivity {
